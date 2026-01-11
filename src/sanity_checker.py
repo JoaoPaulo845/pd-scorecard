adb34148b7c69a930b55fa830c5abb93b96cbf16
JoaoPaulo845 <jp200298@gmail.com>
2026-01-10 21:37:36 -0300

    Adição dos Checks de Exploratory Data Analysis

--- src/sanity_checker.py
+++ src/sanity_checker.py
@@ -12,24 +12,32 @@ DATASET = os.path.join(BASE, "..", "data", "cs-training.csv")
 
 COLUMN_TARGET = "SeriousDlqin2yrs"
 
-#Sanity Checking (Checagem de sanidade) - Visa verificar em menos de 30 segundas se uma aplicação está funcionando, nesse contexto, estou verificando a integridade desse dataset
+# Sanity Checking (Checagem de sanidade) - Visa verificar em menos de 30 segundas se uma aplicação está funcionando, nesse contexto, estou verificando a integridade desse dataset
 
 def main():
     df = pd.read_csv(DATASET)
 
     print("Shape (linhas, colunas):", df.shape)
     # Shape (linhas, colunas): (150000, 12)
+    
     print("\nColunas:")
     print(df.columns.tolist())
-
+    #['Unnamed: 0', 'SeriousDlqin2yrs', 'RevolvingUtilizationOfUnsecuredLines', 'age', 'NumberOfTime30-59DaysPastDueNotWorse', 'DebtRatio', 'MonthlyIncome', 'NumberOfOpenCreditLinesAndLoans', 'NumberOfTimes90DaysLate', 'NumberRealEstateLoansOrLines', 'NumberOfTime60-89DaysPastDueNotWorse', 'NumberOfDependents']
+    
     print("\nTarget distribution (Distribuição de inadimplência):")
     print(df[COLUMN_TARGET].value_counts(dropna=False))
-    #
+    # 0 - 139974
+    # 1 - 10026
+
     print("\nTarget rate (Média):", df[COLUMN_TARGET].mean())
+    # Target rate (Média): 0.06684
 
     print("\nMissing values (Relação de nulos por Coluna):")
     miss = df.isna().sum().sort_values(ascending=False)
-    print(miss.head(10))
+    print(miss.head(3))
+    # MonthlyIncome                           29731
+    # NumberOfDependents                       3924
+    # Unnamed: 0                                  0
 
     print("\Top 3 lines:")
     print(df.head(3))
